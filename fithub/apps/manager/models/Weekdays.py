b9605bfc79c424e7f74777d64ea1315f2b48c0fc
RochaGabriell <rochagabriell.dev@gmail.com>
2023-10-31 14:14:13 -0300

    Pencil: Fix typos.

--- fithub/apps/manager/models/Weekdays.py
+++ fithub/apps/manager/models/Weekdays.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class weekdays(models.Model):
+class Weekdays(models.Model):
 
     name = models.CharField(
         verbose_name=_('Nome do dia da semana'),
