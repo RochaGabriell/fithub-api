a1aef6c68e75a8d688d57e897c24b9a72dbfa30f
RochaGabriell <rochagabriell.dev@gmail.com>
2023-12-12 11:01:23 -0300

    New EndPoint

--- fithub/apps/manager/views/WorkoutDayViewSet.py
+++ fithub/apps/manager/views/WorkoutDayViewSet.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, permissions
 from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
@@ -14,6 +15,8 @@ class WorkoutDayViewSet(viewsets.ModelViewSet):
     serializer_class = WorkoutDaySerializer
     permission_classes = [permissions.IsAuthenticated]
     pagination_class = None
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['workout']
 
     def perform_create(self, serializer):
         if WorkoutDay.objects.filter(workout=serializer.validated_data["workout"], day=serializer.validated_data["day"]).exists():
@@ -22,7 +25,18 @@ class WorkoutDayViewSet(viewsets.ModelViewSet):
         serializer.save()
 
     def perform_update(self, serializer):
-        if WorkoutDay.objects.filter(workout=serializer.validated_data["workout"], day=serializer.validated_data["day"]).exists():
-            raise serializers.ValidationError(
-                _("Já existe um treino para este dia da semana."))
+        instance = self.get_object()
+
+        if "day" in serializer.validated_data and instance.day != serializer.validated_data["day"]:
+            workout = serializer.validated_data["workout"]
+            day = serializer.validated_data["day"]
+
+            if WorkoutDay.objects.filter(workout=workout, day=day).exists():
+                raise serializers.ValidationError(
+                    _("Já existe um treino para este dia da semana.")
+                )
+
         serializer.save()
+
+    def get_queryset(self):
+        return WorkoutDay.objects.filter(workout__user=self.request.user)
--- fithub/apps/manager/views/WorkoutViewSet.py
+++ fithub/apps/manager/views/WorkoutViewSet.py
@@ -2,7 +2,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import viewsets, permissions
 from django.utils.translation import gettext_lazy as _
-from rest_framework import serializers
+from rest_framework import serializers, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from fithub.apps.manager.serializers import WorkoutSerializer
 from fithub.apps.manager.models import Workout
@@ -31,5 +33,31 @@ class WorkoutViewSet(viewsets.ModelViewSet):
                 _("Já existe um treino padrão para este usuário."))
         serializer.save()
 
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update', 'destroy']:
+            self.permission_classes = [permissions.IsAuthenticated, ]
+        return super(WorkoutViewSet, self).get_permissions()
+
     def get_queryset(self):
         return Workout.objects.filter(user=self.request.user) | Workout.objects.filter(public=True)
+
+    @action(detail=False, methods=['get'])
+    def my(self, request):
+        """
+        Endpoint da API que permite que os treinos do usuário sejam visualizados. (GET)
+        """
+        queryset = Workout.objects.filter(user=request.user)
+        serializer = WorkoutSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'])
+    def default(self, request):
+        """
+        Endpoint da API que permite que o treino padrão seja visualizado. (GET)
+        """
+        workout = Workout.objects.filter(user=request.user, default=True)
+        if workout.exists():
+            serializer = WorkoutSerializer(workout[0])
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
