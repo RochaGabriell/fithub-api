0d4676d7614d4e902417d890899ae2ea621561ba
RochaGabriell <rochagabriell.dev@gmail.com>
2023-12-15 15:47:09 -0300

    Time

--- fithub/settings.py
+++ fithub/settings.py
@@ -93,7 +93,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACESSTOKEN_LIFETIME': timedelta(minutes=5),
+    'ACESSTOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
